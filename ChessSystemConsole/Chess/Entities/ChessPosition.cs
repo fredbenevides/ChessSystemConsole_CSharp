2d5f2133f70d103d90bffea47d0bcdfa65776346
Frederico Benevides <fhfreitas@gmail.com>
2019-05-13 22:46:15 -0400

    Created class ChessPosition

---
+++ ChessSystemConsole/Chess/Entities/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GenericBoard.Entities.Enums;
+using GenericBoard.Entities;
+
+namespace Chess.Entities
+{
+    public class ChessPosition
+    {
+        public char collumn { get; set; }
+        public int range { get; set; }
+
+        public ChessPosition(char collumn, int range)
+        {
+            this.collumn = collumn;
+            this.range = range;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - range, collumn - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + collumn + range;
+        }
+    }
+}
